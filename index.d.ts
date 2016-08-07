397faed9e2473cea88572d5cdf45e9a009959a12
Nathan Walker <walkerrunpdx@gmail.com>
2016-08-06 20:58:08 -0700

    chore: fix typings issues

---
+++ index.d.ts
@@ -0,0 +1,57 @@
+export interface AudioPlayerOptions {
+    audioFile: string;
+    completeCallback?: Function;
+    errorCallback?: Function;
+    infoCallback?: Function;
+}
+export interface AudioRecorderOptions {
+    filename: string;
+    maxDuration?: number;
+    errorCallback?: Function;
+    infoCallback?: Function;
+}
+export interface TNSPlayerI {
+    playFromFile(options: AudioPlayerOptions): Promise<any>;
+    playFromUrl(options: AudioPlayerOptions): Promise<any>;
+    play(): Promise<boolean>;
+    pause(): Promise<boolean>;
+    dispose(): Promise<boolean>;
+    isAudioPlaying(): boolean;
+    getAudioTrackDuration(): Promise<string>;
+}
+export interface TNSRecordI {
+    start(options: AudioRecorderOptions): Promise<any>;
+    stop(): Promise<any>;
+    dispose(): Promise<any>;
+}
+export declare class TNSPlayer {
+    static ObjCProtocols: any[];
+    private _player;
+    private _task;
+    private _completeCallback;
+    private _errorCallback;
+    private _infoCallback;
+    playFromFile(options: AudioPlayerOptions): Promise<any>;
+    playFromUrl(options: AudioPlayerOptions): Promise<any>;
+    pause(): Promise<any>;
+    play(): Promise<any>;
+    dispose(): Promise<any>;
+    isAudioPlaying(): boolean;
+    getAudioTrackDuration(): Promise<string>;
+    audioPlayerDidFinishPlayingSuccessfully(player?: any, flag?: boolean): void;
+    private reset();
+}
+export declare class TNSRecorder {
+    static ObjCProtocols: any[];
+    private _recorder;
+    private _recordingSession;
+    static CAN_RECORD(): boolean;
+    start(options: AudioRecorderOptions): Promise<any>;
+    stop(): Promise<any>;
+    dispose(): Promise<any>;
+    isRecording(): any;
+    getMeters(channel: number): any;
+    audioRecorderDidFinishRecording(recorder: any, success: boolean): void;
+}
+
+
