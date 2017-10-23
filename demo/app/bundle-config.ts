530422c0cca531815e2b6d0cef0c7f5d533f0169
Brad Martin <bradwaynemartin@gmail.com>
2017-10-22 20:23:44 -0500

    fix bundle

--- demo/app/bundle-config.ts
+++ demo/app/bundle-config.ts
@@ -4,10 +4,9 @@ if ((<any>global).TNS_WEBPACK) {
   require("tns-core-modules/bundle-entry-points");
 
   global.registerModule("main-page", () => require("./main-page"));
-  global.registerModule("multifab", () => require("./multifab"));
 
   // register application modules
-  global.registerModule("nativescript-floatingactionbutton", () =>
-    require("nativescript-floatingactionbutton")
+  global.registerModule("nativescript-audio", () =>
+    require("nativescript-audio")
   );
 }
